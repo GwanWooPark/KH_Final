05dbb788f7ea189f8782cc2cc2ae55c40e6e52b2
GwanWooPark <pgw4712@icloud.com>
2021-07-28 02:42:13 +0900

    final

--- src/main/java/com/kh/bnpp/model/biz/ReplyBizImpl.java
+++ src/main/java/com/kh/bnpp/model/biz/ReplyBizImpl.java
@@ -50,4 +50,5 @@ public class ReplyBizImpl implements ReplyBiz {
 		return dao.rboard_delete(br_num);
 	}
 
+
 }
--- src/main/java/com/kh/bnpp/model/biz/RtcBizImpl.java
+++ src/main/java/com/kh/bnpp/model/biz/RtcBizImpl.java
@@ -27,4 +27,5 @@ public class RtcBizImpl implements RtcBiz{
     public int compareInfo(String roomId, String roomPass) {
         return dao.compareInfo(roomId, roomPass);
     }
+
 }
